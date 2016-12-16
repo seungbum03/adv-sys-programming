2d5fd6f79c51dc671fa41f5ed72644570a851d00
Seungbeom Shim <ssb3258@naver.com>
2016-12-17 03:33:09 +0900

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "start"
+./chat2 c 01 &
+./chat2 c 02 &
+./chat2 c 03 &
+./chat2 c 04 &
+./chat2 c 05 &
+./chat2 c 06 &
+./chat2 c 07 &
+./chat2 c 08 &
+./chat2 c 09 &
+./chat2 c 10 &
+./chat2 c 11 &
+./chat2 c 12 &
+./chat2 c 13 &
+./chat2 c 14 &
+./chat2 c 15 &
+./chat2 c 16 &
+./chat2 c 17 &
+./chat2 c 18 &
+./chat2 c 19 &
+./chat2 c 20 &
+echo "finish"
